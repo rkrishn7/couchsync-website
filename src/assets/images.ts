07c0406398a6f7e3e4dd4edff3a7432474da9950
Rohan Krishnaswamy <rkrishn7@ucsc.edu>
2020-11-08 14:33:52 -0800

    chore: cleanup

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,14 +1,14 @@
 class Images {
   private get base() {
-    return process.env.NODE_ENV === 'production' ? '/couchsync' : '/';
+    return process.env.NODE_ENV === 'production' ? '/couchsync' : '';
   }
 
   get logo() {
-    return this.base + 'couchsync-128.png';
+    return this.base + '/couchsync-128.png';
   }
 
   get appScreenshot() {
-    return this.base + 'app-screenshot.png';
+    return this.base + '/app-screenshot.png';
   }
 };
 
