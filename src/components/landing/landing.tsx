c6cec7c3d991873739221287b23f625b8379678b
Zachary Zulanas <zzulanas@gmail.com>
2020-12-05 12:52:07 -0800

    added lottie and homepage animations

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -1,23 +1,23 @@
-import { Box, Flex, Heading } from 'rebass';
-import Link from 'next/link';
+import { Box, Flex, Heading } from "rebass";
+import Link from "next/link";
+import Lottie from "lottie-react-web";
 
-import styled from 'styles/styled';
-import Images from 'assets/images';
+import styled from "styles/styled";
+import Images from "assets/images";
+import animation from "assets/data.json";
 
-interface LandingProps {
-
-}
+interface LandingProps {}
 
 const Screenshot = styled.img`
   width: 250px;
   height: auto;
 
-  @media(min-width: 700px) {
+  @media (min-width: 700px) {
     width: 650px;
   }
 
-  border-radius: ${p => p.theme.radii[2]}px;
-  box-shadow: 10px 10px 19px -6px rgba(0,0,0,0.75);
+  border-radius: ${(p) => p.theme.radii[2]}px;
+  box-shadow: 10px 10px 19px -6px rgba(0, 0, 0, 0.75);
 `;
 
 const Container = styled(Flex)`
@@ -36,18 +36,22 @@ const Section = styled(Box)`
 
 export const Landing: React.FC<LandingProps> = () => {
   return (
-    <Container px={5} flexWrap='wrap'>
-      <Section width={[1, 1, 1/2]} px={2}>
-        <Heading fontSize={[ 3, 4, 5 ]} color="primary" fontWeight="600">
+    <Container px={5} flexWrap="wrap">
+      <Section width={[1, 1, 1 / 3]} px={2}>
+        <Heading fontSize={[3, 4, 5]} color="primary" fontWeight="600">
           the better way to stream with friends
         </Heading>
-        <Heading fontSize={[ 1, 2, 3 ]} color="primary" mt={1} fontWeight={200}>
+        <Heading fontSize={[1, 2, 3]} color="primary" mt={1} fontWeight={200}>
           couchsync makes it easier than ever to sync videos in real time
         </Heading>
       </Section>
-      <Section width={[1, 1, 1/2]} px={2}>
-        <Screenshot src={Images.appScreenshot} />
+      <Section width={[1, 1, 2 / 3]} px={0.2}>
+        <Lottie
+          options={{
+            animationData: animation,
+          }}
+        />
       </Section>
     </Container>
   );
-}
+};
